4dd00943043d88e31f1e2cedf175dabc20ddb086
Helly Guo <buffoonguo@gmail.com>
2016-04-09 16:10:57 +0800

    删除除proxy外代码
    On branch devel
    	modified:   src/Makefile.inc
    Untracked files:
    	src/pstdint.h
    no changes added to commit (use "git add" and/or "git commit -a")
    On branch devel
    	deleted:    Makefile.Solaris
    Changes not staged for commit:
    	modified:   src/Makefile.inc
    Untracked files:
    	src/tags
    On branch devel
    	deleted:    cfg/0.scenario.txt
    Untracked files:
    	src/tags
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/plugins/LdapPlugin/com-unix.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-gcc   -shared -o libldapauth.so ldapauth_new.c  -DNOODBC -I/usr/include  -L/usr/lib -lldap 
-
--- src/plugins/LdapPlugin/com-win32.bat
+++
@@ -1,4 +0,0 @@
-del ldapauth.dll
-gcc  -o getldapuser getldapuser.c -DWIN32  -I"./ldapwindev" -L"./ldapwindev" -lldap
-gcc  -shared -o ldapauth.dll ldapauth.c -DWIN32 -I"./ldapwindev" -L"./ldapwindev" -lldap
-
