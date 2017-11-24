747828d3ab828c927251c0f90362b04b4c5ef9d0
z3APA3A <3APA3A@3proxy.ru>
2017-11-24 12:41:01 +0300

    Move sys/timeb.h to Win32 section.

--- src/proxy.h
+++ src/proxy.h
@@ -21,7 +21,6 @@
 #include <ctype.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/timeb.h>
 #include <fcntl.h>
 #include <time.h>
 
@@ -37,6 +36,7 @@
 
 #ifdef _WIN32
 #include <winsock2.h>
+#include <sys/timeb.h>
 #ifndef _WINCE
 #include <io.h>
 #else
