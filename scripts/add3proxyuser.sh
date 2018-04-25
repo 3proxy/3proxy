f860ea9e5446bad7df649aab3bf37430a5895794
z3APA3A <3APA3A@3proxy.ru>
2018-04-25 17:14:03 +0300

    Install chrooted configuration with make install on linux

--- scripts/add3proxyuser.sh
+++ scripts/add3proxyuser.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ $4 ]; then  
-	echo bandlimin $4 $1 >> /etc/3proxy/bandlimiters
+	echo bandlimin $4 $1 >> /etc/3proxy/conf/bandlimiters
 fi
 if [ $3 ]; then  
-	echo countin \"`wc -l /etc/3proxy/counters|awk '{print $1}'`/$1\" D $3 $1 >> /etc/3proxy/counters
+	echo countin \"`wc -l /etc/3proxy/conf/counters|awk '{print $1}'`/$1\" D $3 $1 >> /etc/3proxy/conf/counters
 fi
 if [ $2 ]; then  
-	echo $1:`/bin/mycrypt $$ $2` >> /etc/3proxy/passwd
+	echo $1:`/bin/mycrypt $$ $2` >> /etc/3proxy/conf/passwd
 else
 	echo usage: $0 username password [day_limit] [bandwidth]
 	echo "	"day_limit - traffic limit in MB per day
