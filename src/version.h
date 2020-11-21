bab333556d7585eec4aad6d3515df8f03a06b543
z3APA3A <3APA3A@3proxy.ru>
2020-11-22 01:21:43 +0300

    make 3proxy.rc non version-dependant

--- src/version.h
+++ src/version.h
@@ -4,3 +4,9 @@
 #ifndef BUILDDATE
 #define BUILDDATE ""
 #endif
+#define MAJOR3PROXY 10
+#define SUBMAJOR3PROXY 0
+#define MINOR3PROXY 0
+#define SUBMINOR3PROXY 0
+#define RELEASE3PROXY "3proxy-10-devel\0"
+#define YEAR3PROXY "2020"
