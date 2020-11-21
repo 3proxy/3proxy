72923879a3267017db556e9bb2ba17111a94044c
z3APA3A <3APA3A@3proxy.ru>
2020-11-21 18:36:39 +0300

    Make rc file non-version specific

--- src/version.h
+++ src/version.h
@@ -4,3 +4,9 @@
 #ifndef BUILDDATE
 #define BUILDDATE ""
 #endif
+#define MAJOR3PROXY 0
+#define SUBMAJOR3PROXY 9
+#define MINOR3PROXY 2
+#define SUBMINOR3PROXY 0
+#define RELEASE3PROXY "3proxy-0.9.2\0"
+#define YEAR3PROXY "2020"
