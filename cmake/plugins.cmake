71d676eb585a4c836306b33d141b40bd5bb1b6f2
Vladimir Dubrovin <3proxy@3proxy.ru>
2026-04-09 17:53:27 +0300

    Update workflows and builds

--- cmake/plugins.cmake
+++ cmake/plugins.cmake
@@ -4,30 +4,6 @@
 # This file defines functions for building plugins
 #
 
-# Function to add a simple plugin (single source file, no dependencies)
-function(add_3proxy_plugin_simple PLUGIN_NAME SOURCE_FILE)
-    if(WIN32)
-        set(PLUGIN_SUFFIX ".dll")
-    else()
-        set(PLUGIN_SUFFIX ".ld.so")
-    endif()
-
-    add_library(${PLUGIN_NAME} SHARED ${SOURCE_FILE})
-
-    set_target_properties(${PLUGIN_NAME} PROPERTIES
-        PREFIX ""
-        SUFFIX ${PLUGIN_SUFFIX}
-        LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin
-        RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin
-    )
-
-    target_link_libraries(${PLUGIN_NAME} PRIVATE Threads::Threads)
-
-    target_include_directories(${PLUGIN_NAME} PRIVATE
-        ${CMAKE_SOURCE_DIR}/src
-    )
-endfunction()
-
 # Function to add a plugin with dependencies
 function(add_3proxy_plugin PLUGIN_NAME)
     set(options "")
