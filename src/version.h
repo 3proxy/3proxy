ff3398c6a93cdf1f598d4633d66aef072d904a96
Vladimir Dubrovin <3proxy@3proxy.ru>
2026-08-20 18:32:25 +0300

    Commit as 0.9.9

--- src/version.h
+++ src/version.h
@@ -1,14 +1,14 @@
 #ifndef VERSION
-#define VERSION "3proxy-0.9.8"
+#define VERSION "3proxy-0.9.9"
 #endif
 #ifndef BUILDDATE
 #define BUILDDATE ""
 #endif
 #define MAJOR3PROXY 0
 #define SUBMAJOR3PROXY 9
-#define MINOR3PROXY 8
+#define MINOR3PROXY 9
 #define SUBMINOR3PROXY 0
-#define RELEASE3PROXY "3proxy-0.9.8(" BUILDDATE ")\0"
+#define RELEASE3PROXY "3proxy-0.9.9(" BUILDDATE ")\0"
 #ifndef YEAR3PROXY
 #define YEAR3PROXY "2026"
 #endif
